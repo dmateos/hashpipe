8165e1890fb45a93f509e2d72db4438ce49e8902
Daniel Mateos <daniel@mateos.cc>
2019-11-30 04:13:23 +1100

    refactor

--- queuemanager/tests/test_models.py
+++ queuemanager/tests/test_models.py
@@ -2,6 +2,7 @@ from queuemanager.models import Endpoint
 from threading import Thread
 import time
 
+
 def test_endpoint_pushes_to_queue_with_redis_engine():
     endpoint = Endpoint(engine="REDIS", ep_id="abc123")
     v = endpoint.push(20)
@@ -12,16 +13,15 @@ def test_endpoint_pull_from_queue_with_redis_engine():
     endpoint = Endpoint(engine="REDIS", ep_id="abc123")
     v = endpoint.pull()
 
-    thread = Thread(target=_run_endpoint_push, args=(endpoint,))
+    def _endpoint_push(endpoint):
+        # Bit hacky but we sleep to give bellow time
+        # to listen.
+        time.sleep(0.1)
+        endpoint.push("hello")
+        endpoint.push("world")
+
+    thread = Thread(target=_endpoint_push, args=(endpoint,))
     thread.start()
 
     assert next(v) == "hello"
     assert next(v) == "world"
-
-    thread.join()
-
-
-def _run_endpoint_push(endpoint):
-    time.sleep(0.1)
-    endpoint.push("hello")
-    endpoint.push("world")
